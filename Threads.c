2b417bce3f18e27beb4ddb90934348545788160e
KAVIN-KJ <kavinkj2004@gmail.com>
2025-11-30 23:18:25 +0530

    Ropes :)

--- Threads.c
+++ Threads.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 #include <pthread.h>
 #include <unistd.h>
-
+// WHAT'S A STRUCT OBJECT AND POINTER TO A STRUCT ? 
+void* task1(void* arg){
+    while(1){
+        printf("Task 1 is running\n");
+        sleep(1);
+    }
+    return NULL;
+}
+void* task2(void* arg){
+    while(1){
+        printf("Task 2 is running\n");
+        sleep(2);
+    }
+    return NULL;
+}
 int main(){
-    
+    pthread_t t1,t2;
+
+    pthread_create(&t1, NULL, task1, NULL);
+    pthread_create(&t2, NULL, task2, NULL);
+
+    pthread_join(t1,NULL);
+    pthread_join(t2,NULL);
 }
